c7c66b2ad1ed834102f4804c16778a7b45101afa
AD-SYS\y50039 <hanamori-yutaro@caica.jp>
2025-08-27 15:34:50 +0900

    Unsupported DID methodの文言変更

--- src/services/resolver.js
+++ src/services/resolver.js
@@ -9,5 +9,5 @@ export async function universalResolve(did) {
   if (did.startsWith('did:ethr:')) {
     return await resolveDidEthr(did);
   }
-  throw new Error('Unsupported DID method');
+  throw new Error('未対応のDIDです。');
 }
